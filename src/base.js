f93984adbada1b450a99dfccedf3d82cde555fa3
Tubshinbat~ <tuvshinbat596@gmail.com>
2024-01-31 12:37:01 +0800

    base change

--- src/base.js
+++ src/base.js
@@ -3,7 +3,7 @@ const base = {
   // baseUrl: "http://localhost:3001/",
   // cdnUrl: "https://naog.lvg.mn/rest/uploads/",
   // apiUrl: "http://localhost:2004/v1/api/",
-  cdnUrl: "https://adw.naog.edu.mn/uploads/",
+  cdnUrl: "https://naog.edu.mn/uploads/",
   baseUrl: "https://adw.naog.edu.mn/",
   siteUrl: "https://naog.edu.mn/",
   apiUrl: "https://adw.naog.edu.mn/api/",
